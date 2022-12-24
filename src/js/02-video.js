479143c2c5b0ea5301a03384b908bb4a426e0117
Olga-Melnyk <3717129@ukr.net>
2022-12-24 18:35:21 +0200

    correction

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,12 +3,15 @@ import throttle from 'lodash.throttle';
 // console.log(Player);
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
 }
 
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+console.log(
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+);
