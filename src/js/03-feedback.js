b2ec4f74f7ee1b6478697b75a6a444b12679767d
Olga-Melnyk <3717129@ukr.net>
2022-12-28 19:20:22 +0200

    test-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,7 +3,7 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('.feedback-form input');
 const message = document.querySelector('.feedback-form textarea');
-console.dir(form);
+// console.dir(form);
 
 const formKey = 'feedback-form-state';
 const data = JSON.parse(localStorage.getItem(formKey));
@@ -15,10 +15,12 @@ form.addEventListener('input', throttle(onInput, 500));
 form.addEventListener('submit', onSubmit);
 
 function onInput(evt) {
-  const savedObject = JSON.parse(localStorage.getItem(formKey));
-  object = savedObject;
-  object[evt.target.name] = evt.target.value;
-  localStorage.setItem(formKey, JSON.stringify(object));
+  if (data) {
+    object = data;
+  } else {
+    object[evt.target.name] = evt.target.value;
+    localStorage.setItem(formKey, JSON.stringify(object));
+  }
 }
 
 function onSubmit(evt) {
